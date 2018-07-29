4dcc43a9c36080cab71871dd01bb9866e3abcc7d
fabiolindemberg <fabiolindemberg@gmail.com>
2018-07-29 07:56:15 -0300

    Adicionando controle de excecao e tratamento para categoria nao encontrada

--- src/main/java/com/fabiolindemberg/cursomc/services/CategoriaService.java
+++ src/main/java/com/fabiolindemberg/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.fabiolindemberg.cursomc.domain.Categoria;
 import com.fabiolindemberg.cursomc.repositories.CategoriaRepository;
+import com.fabiolindemberg.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,6 +18,6 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Categoria nao encontrada!"));
 	}
 }
